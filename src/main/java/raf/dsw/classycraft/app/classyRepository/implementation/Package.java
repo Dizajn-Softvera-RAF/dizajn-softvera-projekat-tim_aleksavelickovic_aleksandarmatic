de292a43fa96897163cdd96b277a173830a69eb2
avelickovic <infin1te@protonmail.com>
2023-10-29 18:06:59 +0100

    Mala izmena u Project

--- src/main/java/raf/dsw/classycraft/app/classyRepository/implementation/Package.java
+++ src/main/java/raf/dsw/classycraft/app/classyRepository/implementation/Package.java
@@ -3,4 +3,6 @@ package raf.dsw.classycraft.app.classyRepository.implementation;
 import raf.dsw.classycraft.app.classyRepository.composite.ClassyNodeComposite;
 
 public class Package extends ClassyNodeComposite {
+    Project p=new Project();
+
 }
--- src/main/java/raf/dsw/classycraft/app/classyRepository/implementation/Project.java
+++ src/main/java/raf/dsw/classycraft/app/classyRepository/implementation/Project.java
@@ -1,8 +1,34 @@
 package raf.dsw.classycraft.app.classyRepository.implementation;
 
+import lombok.Getter;
 import raf.dsw.classycraft.app.classyRepository.composite.ClassyNodeComposite;
-
+@Getter
 public class Project extends ClassyNodeComposite {
-    String autorName;
-    String path;
+    public String getAutorName() {
+        return autorName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setAutorName(String autorName) {
+        this.autorName = autorName;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    private String autorName;
+    private String name;
+    private String path;
 }
