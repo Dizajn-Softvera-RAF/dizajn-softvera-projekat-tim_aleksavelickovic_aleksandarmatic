ff0bbce852b61a7219f0cb511e43384a6834beb8
avelickovic <infin1te@protonmail.com>
2023-10-29 22:16:54 +0100

    Zavrsen message generator i dodat gitignore

--- src/main/java/raf/dsw/classycraft/app/AppCore.java
+++ src/main/java/raf/dsw/classycraft/app/AppCore.java
@@ -1,18 +1,12 @@
 package raf.dsw.classycraft.app;
 
-import raf.dsw.classycraft.app.classyRepository.implementation.Diagram;
-import raf.dsw.classycraft.app.classyRepository.implementation.Package;
-import raf.dsw.classycraft.app.classyRepository.implementation.ProjectExplorer;
+
 import raf.dsw.classycraft.app.core.ApplicationFramework;
 
 public class AppCore {
     public static void main(String[] args) {
         ApplicationFramework appCore = ApplicationFramework.getInstance();
         appCore.initialize();
-       // Diagram diagram=new Diagram("dija",null);
-       // Package paket =new Package("paket",null);
-       /// ProjectExplorer projectExplorer=new ProjectExplorer();
-        //projectExplorer.addChild(paket);
-       // appCore.getClassyRepositoryImplementation().getRoot().addChild(diagram);
+
     }
 }
