90e028623c67a6e3de527795c1a0e2da6eeaef46
avelickovic <avelickovic8823rn@raf.rs>
2023-12-03 11:22:36 +0100

    Dodat updejt za dodavanje metoda u interface i dodvanje classcontenta u clasu

--- src/main/java/raf/dsw/classycraft/app/classyRepository/diagramElementImplementation/diagramelements/Class.java
+++ src/main/java/raf/dsw/classycraft/app/classyRepository/diagramElementImplementation/diagramelements/Class.java
@@ -4,17 +4,22 @@ import raf.dsw.classycraft.app.classyRepository.composite.ClassyNode;
 import raf.dsw.classycraft.app.classyRepository.diagramElementImplementation.AccessModifier;
 import raf.dsw.classycraft.app.classyRepository.diagramElementImplementation.DiagramElement;
 import raf.dsw.classycraft.app.classyRepository.diagramElementImplementation.InterClass;
+import raf.dsw.classycraft.app.core.observer.Pubsliher;
+import raf.dsw.classycraft.app.core.observer.Subscriber;
+import raf.dsw.classycraft.app.core.observer.interCommunicationNotification.InterCommunicationNotification;
 
 import java.awt.*;
 import java.util.ArrayList;
 
-public  class Class extends InterClass {
+public  class Class extends InterClass implements Pubsliher {
     private ArrayList<ClassContents> classContents;
+    private ArrayList<Subscriber> subscribers;
 
 
     public Class(String name, ClassyNode parent, Color color, Stroke stroke, AccessModifier accessModifier, Point postition, Dimension size) {
         super(name, parent, color, stroke, accessModifier, postition, size);
         classContents = new ArrayList<>();
+        subscribers = new ArrayList<>();
 
     }
 
@@ -25,5 +30,14 @@ public  class Class extends InterClass {
 
     public void setClassContents(ArrayList<ClassContents> classContents) {
         this.classContents = classContents;
+    }
+    public void addClassContents(ClassContents classContent){
+
+        classContents.add(classContent);
+        notifySubscribers(new InterCommunicationNotification("CLASSCONTENT_ADDED"));
+    }
+    public void removeClassContents(ClassContents classContent){
+        classContents.remove(classContent);
+        notifySubscribers(new InterCommunicationNotification("CLASSCONTENT_REMOVED"));
     }
 }
--- src/main/java/raf/dsw/classycraft/app/classyRepository/diagramElementImplementation/diagramelements/Interface.java
+++ src/main/java/raf/dsw/classycraft/app/classyRepository/diagramElementImplementation/diagramelements/Interface.java
@@ -4,16 +4,22 @@ import raf.dsw.classycraft.app.classyRepository.composite.ClassyNode;
 import raf.dsw.classycraft.app.classyRepository.diagramElementImplementation.AccessModifier;
 import raf.dsw.classycraft.app.classyRepository.diagramElementImplementation.DiagramElement;
 import raf.dsw.classycraft.app.classyRepository.diagramElementImplementation.InterClass;
+import raf.dsw.classycraft.app.core.observer.Pubsliher;
+import raf.dsw.classycraft.app.core.observer.Subscriber;
+import raf.dsw.classycraft.app.core.observer.interCommunicationNotification.InterCommunicationNotification;
 
 import java.awt.*;
 import java.util.ArrayList;
 
-public  class Interface extends InterClass {
+public  class  Interface extends InterClass implements Pubsliher {
     private ArrayList<ClassContents> methods;
-
+    private ArrayList<Subscriber> subscribers;
 
     public Interface(String name, ClassyNode parent, Color color, Stroke stroke, AccessModifier accessModifier, Point postition, Dimension size) {
         super(name, parent, color, stroke, accessModifier, postition, size);
+
+        methods=new ArrayList<>();
+        subscribers = new ArrayList<>();
 
     }
 
@@ -21,8 +27,28 @@ public  class Interface extends InterClass {
         return methods;
     }
 
-    public void setMethods(ArrayList<ClassContents> methods) {
-        this.methods = methods;
+    public void addMethods(Method method){
+        methods.add(method);
+        notifySubscribers(new InterCommunicationNotification("METHOD_ADDED"));
+    }
+    public void removeMethods(Method method){
+        methods.remove(method);
+        notifySubscribers(new InterCommunicationNotification("METHOD_REMOVED"));
+    }
+
+    public void addSubscriber(Subscriber subscriber) {
+        subscribers.add(subscriber);
+    }
+
+    @Override
+    public void removeSubscriber(Subscriber subscriber) {
+        subscribers.remove(subscriber);
+    }
+
+    @Override
+    public void notifySubscribers(Object notification) {
+        for(Subscriber subscriber:subscribers)
+            subscriber.update(notification);
     }
 
 }
